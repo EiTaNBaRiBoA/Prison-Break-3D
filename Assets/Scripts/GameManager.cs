6516f688389a1d1268df6223d99227b605c116b8
EiTaNBaRiBoA <eitanshapirshtein@gmail.com>
2020-05-26 09:08:54 +0300

    Splash Screen working

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
+
+public sealed class GameManager : MonoBehaviour
+{    
+    private static GameManager _instance;
+    public static GameManager gameManager{ get {return _instance;}}
+
+    LevelManager levelManager;
+
+    private void Awake() {
+        if(_instance==null)
+        {
+            _instance=this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        levelManager = FindObjectOfType<LevelManager>();
+        levelManager.LoadingNextScene(5f);
+    }
+
+
+}
