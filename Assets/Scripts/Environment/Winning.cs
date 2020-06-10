bff1bdfef6d63f3bbea29deb79e0d9ee78ca7596
eitanbariboa <eitanshapirshtein@gmail.com>
2020-06-10 19:39:52 +0300

    Added Collision

---
+++ Assets/Scripts/Environment/Winning.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winning : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        Cursor.lockState = CursorLockMode.None;
+        GameManager.gameManager.RequestToLoadScene(2);
+    }
+}
